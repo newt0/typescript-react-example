829073a8a0387ccaaa3b274f4c7f9e243f0a390a
newt0 <py_lambda@yahoo.co.jp>
2020-10-11 15:20:39 +0900

    add: display TextField in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import TextField from "./TextField";
 
 const App: React.FC = () => {
-  return <div>yo</div>;
+  return (
+    <div>
+      <TextField text="text"/>
+    </div>
+  );
 };
 
 export default App;
