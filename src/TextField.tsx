0dcceba0822cefdcf7db44f01ac32489d7968060
newt0 <py_lambda@yahoo.co.jp>
2020-10-11 15:23:55 +0900

    add: other properties

--- src/TextField.tsx
+++ src/TextField.tsx
@@ -2,6 +2,9 @@ import React from "react";
 
 interface Props {
   text: string;
+  ok: boolean;
+  i: number;
+  fn: () => (void) // function must be arrow function 
 }
 const TextField: React.FC<Props> = () => {
   return (
